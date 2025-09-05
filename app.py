f86817c5e13e295f9cbb6adce0f1fa9b71eb450e
RobertoPerezOne <roberto.perezmorales14@gmail.com>
2025-09-04 22:22:48 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ def index():
     return render_template('index.html', resultados=resultados)
 
 if __name__ == '__main__':
-      port = int(os.environ.get("PORT", 5000))  # usa el puerto de Render o 5000 local
+    port = int(os.environ.get("PORT", 5000))  # usa el puerto de Render o 5000 local
     app.run(host="0.0.0.0", port=port)
